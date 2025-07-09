a8307feb0e147b661336a3a76ecee7d775a6bae3
Lance Joseph Dayawon <lancejosephmanalangdayawonwork@gmail.com>
2025-07-09 19:35:10 +0800

    Update Customer Segmentation & Behavior.sql

--- Projects/SuperStoreAnalysisSQL/SQL Scripts/Customer Segmentation & Behavior.sql
+++ Projects/SuperStoreAnalysisSQL/SQL Scripts/Customer Segmentation & Behavior.sql
@@ -1,10 +1,12 @@
+#Customer Segmentation & Behavior
 SELECT 
-  `Customer ID`, 
-  `Customer Name`,
-  Segment,
-  COUNT(DISTINCT `Order ID`) AS order_count,   # How many separate orders each customer made
-  SUM(Sales) AS total_sales,                   # How much they bought (revenue)
-  SUM(Profit) AS total_profit                  # How much profit they brought in
+	`Customer_ID`, 
+	`Customer_Name`,
+	Segment,
+	COUNT(DISTINCT `Order_ID`) AS order_count,   		    #How many separate orders each customer made
+	ROUND((SUM(Sales)),2) AS total_sales,               #How much they bought (revenue)
+	ROUND((SUM(Profit)),2) AS total_profit              #How much profit they brought in
 FROM orders
-GROUP BY `Customer ID`, `Customer Name`, Segment # Group by each customer + their segment
-ORDER BY total_sales DESC                      # Show top customers by sales
+GROUP BY `Customer_ID`, `Customer_Name`, Segment 		#Group by each customer + their segment
+ORDER BY total_sales DESC                      			#Show top customers by sales
+LIMIT 10;                                      			#Only show the top 10
