55253c5c84195e930a0f8e1a16ae16443ed5cdb4
Lance Joseph Dayawon <lancejosephmanalangdayawonwork@gmail.com>
2025-07-09 19:39:54 +0800

    Update KPI's.sql

--- Projects/SuperStoreAnalysisSQL/SQL Scripts/KPI's.sql
+++ Projects/SuperStoreAnalysisSQL/SQL Scripts/KPI's.sql
@@ -2,5 +2,4 @@ SELECT
     ROUND(SUM(Sales), 2) AS total_sales,
     ROUND(SUM(Profit), 2) AS total_profit,
     ROUND(SUM(Profit)/SUM(Sales), 2) AS overall_profit_margin,
-    ROUND(SUM(Sales)/COUNT(DISTINCT Order_ID), 2) AS avg_order_value
 FROM orders;
